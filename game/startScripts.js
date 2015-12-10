a6c669cc40ce7f3971c41479fd07fd13d48aceaa
JTaylor27 <taylorf27tetete@gmail.com>
2015-12-10 17:21:50 -0500

    first real game

--- game/startScripts.js
+++ game/startScripts.js
@@ -1,17 +1,160 @@
-var myGame = {
-  config : {
+A3D.config = {
+  game:{
+    forceResolution: true,
+    targetRes : [640,480],
+    startScene : 'level1'
+  },
+  scenes : {
+    level1 : {
+      rootUrl : './scenes/',
+      file : 'level1.babylon',
+      onload : function(){
+        A3D.ActiveGame.mainScene.player.hasStick = false;
+        A3D.ActiveGame.mainScene.player.hasSsaw = false;
+        A3D.myGameFunctions = {
+          logtrig : function(){
+            if(A3D.ActiveGame.mainScene.player.hasStick){
+              var logPatrol={
+                speed: 0.02,
+                patrolPoints : [
+                  {
+                    gotoPoint:{x:0.9232,y:0.1315,z:0.1474},
+                    //  gotoPoint:{x:4,y:1,z:4},
+
+                    lookTo:false
+                  }
+                ]
+              }
+              A3D.ActiveGame.mainScene.Actor.log.makePatrol(logPatrol);
+              A3D.ActiveGame.mainScene.Actor.log.startPatrol();
+              console.log('If I can knock over these logs...');
+               A3D.ActiveGame.mainScene.player.hasStick = false;
+          } else {
+            console.log('I need a stick to move theses logs');
+          }
+
+          },
+           enemytrig : function(){
+             console.log('Now I can use the saw');
+              A3D.ActiveGame.mainScene.player.hasSsaw = true;
+           },
+           walltrig : function(){
+             if( A3D.ActiveGame.mainScene.player.hasSsaw){
+               console.log("It's open");
+               A3D.ActiveGame.mainScene.player.hasSsaw = false;
+               A3D.ActiveGame.mainScene.Actor.Barricade.setLocation({x:800,y:800,z:800});
+             } else {
+               console.log('I need something to get past the wood');
+             }
+
+
+           },
+           sticktrig : function(){
+             console.log('I can use this to push the log');
+             A3D.ActiveGame.mainScene.player.hasStick = true;
+             A3D.ActiveGame.mainScene.Actor.stick.setLocation({x:800,y:800,z:800});
+           }
+
+        };
+
+/*
+
+        var triggers = A3D.ActiveGame.mainScene.Trigger
+        console.log(triggers);
+
+        A3D.ActiveGame.myFunction = function(){
+          console.log(this);
+          console.log('YOu just ran into '+this.name)
+        }
+
+        for(var o in triggers){
+          triggers[o].setupActivateAction.func =
+          A3D.ActiveGame.myFunction.bind(triggers[o])
+        }
+
+*/
+
+        var patrol = [];
+        patrol.push({
+          speed: 0.02,
+          patrolPoints : [
+            {
+              gotoPoint:{x:4,y:0,z:-4.5},
+              lookTo:true
+            },
+            {
+              gotoPoint:{x:1,y:0,z:-1.1},
+              lookTo:true
+            },
+            {
+              gotoPoint:{x:7,y:0,z:-2.4},
+              lookTo:true
+            }
+          ]
+        });
+
+        patrol.push({
+          speed: 0.02,
+          patrolPoints : [
+            {
+              gotoPoint:{x:-2,y:0,z:-2},
+              lookTo:true
+            },
+            {
+              gotoPoint:{x:-4,y:0,z:4.4},
+              lookTo:true
+            },
+            {
+              gotoPoint:{x:-0.4,y:0,z:0.2},
+              lookTo:true
+            }
+          ]
+        });
+        patrol.push({
+          speed: 0.02,
+          patrolPoints : [
+            {
+              gotoPoint:{x:-4,y:0,z:-1},
+              lookTo:true
+            },
+            {
+              gotoPoint:{x:2,y:0,z:-5.4},
+              lookTo:true
+            },
+            {
+              gotoPoint:{x:-5,y:0,z:-4.3},
+              lookTo:true
+            }
+          ]
+        });
+        var i = 0;
+        for(var e in A3D.ActiveGame.mainScene.Enemy){
+          console.log(e);
+          if(e != 'boss'){
+            var me = A3D.ActiveGame.mainScene.Enemy[e];
+            me.makePatrol(patrol[i]);
+            me.startPatrol();
+            i++;
+          }
+        }
+        A3D.ActiveGame.runOnAssetsLoaded();
+      }
+    }
+  },
     playerSetup : {
-      urlRoot : './models/',
-      file : 'chris.babylon',
-      importMeshName:'Chris',
+      rootUrl : './models/',
+      file : 'dracobones.babylon',
+      name : 'Draco',
+      importMeshName:'Draco',
+      startSpawn : 'spawn',
       boundsSize : {
-        x : 0.2,
-        y: 0.5,
-        z : 0.2
+        x : .45,
+        y: .45,
+        z : .45
       },
       boundsOffset : {
         x : 0,
-        y: 0.9,
+        y: .9,
         z: 0
       },
       scale : {
@@ -22,13 +165,13 @@ var myGame = {
       animationsMap : [
         {
           name : 'idle',
-          start : 80,
-          end : 100
+          start : 29,
+          end : 30
         },
         {
           name : 'running',
-          start : 50,
-          end : 70
+          start : 1,
+          end : 40
         },
         {
           name : 'walking',
@@ -54,6 +197,20 @@ var myGame = {
         }
       ]
     },
+    screenImages : [
+      {
+        src:'/images/start.png',
+        id : 'mainScreen'
+      },
+      {
+        src:'/images/lose.png',
+        id : 'loseScreen'
+      },
+      {
+        src:'/images/win.png',
+        id : 'winScreen'
+      }
+    ],
 
     targetSetup : {
       rootUrl : './models/',
@@ -84,24 +241,11 @@ var myGame = {
         z:0
       }
     }
-  },
-
-  scripts : function(s){
-    A3D.ActiveGame.run();
-    console.log('The Game Has Begun')
-  //  var p = A3D.ActiveGame.mainScene.player;
-    //p.scale.y = 4;
-  //  p._setScale();
-  }
-};
+  };
 
 
 function start(){
-    new A3D.Game(myGame.config);
-    A3D.ActiveGame._loadScene('./scenes/','level1.babylon',function(){
-      myGame.Adventure = new A3D.Module.Adventure(myGame.config);
-      myGame.Adventure.loadModule(myGame.scripts);
-    });
+  new A3D.Game();
 }
 
 document.addEventListener( "DOMContentLoaded", start, false );
